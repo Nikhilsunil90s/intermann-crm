b9f3ac43f863bba0ace2bd5b02359f9ee8f0c412
Nischay Chandra <chandranischay@gmail.com>
2022-04-28 18:10:44 +0530

    add a client page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
+import AddClient from './pages/AddClient';
 
 function App() {
   return (
     <>
       <Login/>
+      <div className="d-flex">
+        <div className="col-3 bg-secondary"></div>
+        <div className="col-9">
+          <AddClient/>
+        </div>
+      </div>
     </>
   );
 }
